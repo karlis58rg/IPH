41ef6e5f47b64852cb7435f1a3654dccd7d6c54b
profebeny <roman.j200@gmail.com>
2021-05-13 16:44:05 -0500

    Estructura de carpetas para Iph_Administrativo.
    Signed-off-by: profebeny <roman.j200@gmail.com>

---
+++ app/src/main/java/mx/ssp/iph/administrativo/ui/activitys/Iph_Administrativo.java
@@ -0,0 +1,18 @@
+package mx.ssp.iph.administrativo.ui.activitys;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import mx.ssp.iph.R;
+
+public class Iph_Administrativo extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iph_administrativo);
+
+
+    }
+}
