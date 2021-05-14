12a9e72e9f25e28dc16204a100ae675f632d8e85
profebeny <roman.j200@gmail.com>
2021-05-14 09:53:51 -0500

    layout

--- app/src/main/java/mx/ssp/iph/administrativo/ui/fragmets/NoReferencia_Administrativo.java
+++ app/src/main/java/mx/ssp/iph/administrativo/ui/fragmets/NoReferencia_Administrativo.java
@@ -26,7 +26,7 @@ public class NoReferencia_Administrativo extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.no_referencia_fragment, container, false);
+        return inflater.inflate(R.layout.no_referencia_administrativo_fragment, container, false);
     }
 
     @Override
