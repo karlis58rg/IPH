f3f0103b34450ee6aa2fd8456ce57d1778039f21
profebeny <roman.j200@gmail.com>
2021-05-20 10:59:47 -0500

    Login

---
+++ app/src/main/java/mx/ssp/iph/Login.java
@@ -0,0 +1,36 @@
+package mx.ssp.iph;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import mx.ssp.iph.administrativo.ui.activitys.Iph_Administrativo;
+import mx.ssp.iph.principal.ui.activitys.Principal;
+
+public class Login extends AppCompatActivity {
+
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this,Principal.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
