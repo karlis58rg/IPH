f82b82acd35c0dbda544951b761cb81d910c4aee
Karlita <karlis58rg@gmail.com>
2021-07-02 15:41:24 -0500

    vistas_Delictivo_get_post

---
+++ app/src/main/java/mx/ssp/iph/delictivo/model/ModeloHechoDelictivo.java
@@ -0,0 +1,205 @@
+package mx.ssp.iph.delictivo.model;
+
+public class ModeloHechoDelictivo {
+    public String IdHechoDelictivo;
+    public String NumReferencia;
+    public String Fecha;
+    public String Hora;
+    public String NumExpediente;
+    public String AnexoDetenciones;
+    public String NumAnexoDetenciones;
+    public String AnexoUsoFuerza;
+    public String NumAnexoUsoFuerza;
+    public String AnexoVehiculos;
+    public String NumAnexoVehiculo;
+    public String AnexoArmasObjetos;
+    public String NumAnexoArmasObjetos;
+    public String AnexoEntrevista;
+    public String NumAnexoEntrevista;
+    public String AnexoLugarIntervencion;
+    public String NumAnexoLugarIntervencion;
+    public String AnexoNoSeEntregan;
+    public String IdPoliciaPrimerRespondiente;
+
+    public String getIdHechoDelictivo() {
+        return IdHechoDelictivo;
+    }
+
+    public String getNumReferencia() {
+        return NumReferencia;
+    }
+
+    public String getFecha() {
+        return Fecha;
+    }
+
+    public String getHora() {
+        return Hora;
+    }
+
+    public String getNumExpediente() {
+        return NumExpediente;
+    }
+
+    public String getAnexoDetenciones() {
+        return AnexoDetenciones;
+    }
+
+    public String getNumAnexoDetenciones() {
+        return NumAnexoDetenciones;
+    }
+
+    public String getAnexoUsoFuerza() {
+        return AnexoUsoFuerza;
+    }
+
+    public String getNumAnexoUsoFuerza() {
+        return NumAnexoUsoFuerza;
+    }
+
+    public String getAnexoVehiculos() {
+        return AnexoVehiculos;
+    }
+
+    public String getNumAnexoVehiculo() {
+        return NumAnexoVehiculo;
+    }
+
+    public String getAnexoArmasObjetos() {
+        return AnexoArmasObjetos;
+    }
+
+    public String getNumAnexoArmasObjetos() {
+        return NumAnexoArmasObjetos;
+    }
+
+    public String getAnexoEntrevista() {
+        return AnexoEntrevista;
+    }
+
+    public String getNumAnexoEntrevista() {
+        return NumAnexoEntrevista;
+    }
+
+    public String getAnexoLugarIntervencion() {
+        return AnexoLugarIntervencion;
+    }
+
+    public String getNumAnexoLugarIntervencion() {
+        return NumAnexoLugarIntervencion;
+    }
+
+    public String getAnexoNoSeEntregan() {
+        return AnexoNoSeEntregan;
+    }
+
+    public String getIdPoliciaPrimerRespondiente() {
+        return IdPoliciaPrimerRespondiente;
+    }
+
+    public void setIdHechoDelictivo(String idHechoDelictivo) {
+        IdHechoDelictivo = idHechoDelictivo;
+    }
+
+    public void setNumReferencia(String numReferencia) {
+        NumReferencia = numReferencia;
+    }
+
+    public void setFecha(String fecha) {
+        Fecha = fecha;
+    }
+
+    public void setHora(String hora) {
+        Hora = hora;
+    }
+
+    public void setNumExpediente(String numExpediente) {
+        NumExpediente = numExpediente;
+    }
+
+    public void setAnexoDetenciones(String anexoDetenciones) {
+        AnexoDetenciones = anexoDetenciones;
+    }
+
+    public void setNumAnexoDetenciones(String numAnexoDetenciones) {
+        NumAnexoDetenciones = numAnexoDetenciones;
+    }
+
+    public void setAnexoUsoFuerza(String anexoUsoFuerza) {
+        AnexoUsoFuerza = anexoUsoFuerza;
+    }
+
+    public void setNumAnexoUsoFuerza(String numAnexoUsoFuerza) {
+        NumAnexoUsoFuerza = numAnexoUsoFuerza;
+    }
+
+    public void setAnexoVehiculos(String anexoVehiculos) {
+        AnexoVehiculos = anexoVehiculos;
+    }
+
+    public void setNumAnexoVehiculo(String numAnexoVehiculo) {
+        NumAnexoVehiculo = numAnexoVehiculo;
+    }
+
+    public void setAnexoArmasObjetos(String anexoArmasObjetos) {
+        AnexoArmasObjetos = anexoArmasObjetos;
+    }
+
+    public void setNumAnexoArmasObjetos(String numAnexoArmasObjetos) {
+        NumAnexoArmasObjetos = numAnexoArmasObjetos;
+    }
+
+    public void setAnexoEntrevista(String anexoEntrevista) {
+        AnexoEntrevista = anexoEntrevista;
+    }
+
+    public void setNumAnexoEntrevista(String numAnexoEntrevista) {
+        NumAnexoEntrevista = numAnexoEntrevista;
+    }
+
+    public void setAnexoLugarIntervencion(String anexoLugarIntervencion) {
+        AnexoLugarIntervencion = anexoLugarIntervencion;
+    }
+
+    public void setNumAnexoLugarIntervencion(String numAnexoLugarIntervencion) {
+        NumAnexoLugarIntervencion = numAnexoLugarIntervencion;
+    }
+
+    public void setAnexoNoSeEntregan(String anexoNoSeEntregan) {
+        AnexoNoSeEntregan = anexoNoSeEntregan;
+    }
+
+    public void setIdPoliciaPrimerRespondiente(String idPoliciaPrimerRespondiente) {
+        IdPoliciaPrimerRespondiente = idPoliciaPrimerRespondiente;
+    }
+
+
+    public ModeloHechoDelictivo(String idHechoDelictivo, String numReferencia, String fecha, String hora,
+                                String numExpediente, String anexoDetenciones, String numAnexoDetenciones,
+                                String anexoUsoFuerza, String numAnexoUsoFuerza, String anexoVehiculos,
+                                String numAnexoVehiculo, String anexoArmasObjetos, String numAnexoArmasObjetos,
+                                String anexoEntrevista, String numAnexoEntrevista, String anexoLugarIntervencion,
+                                String numAnexoLugarIntervencion, String anexoNoSeEntregan,
+                                String idPoliciaPrimerRespondiente) {
+        IdHechoDelictivo = idHechoDelictivo;
+        NumReferencia = numReferencia;
+        Fecha = fecha;
+        Hora = hora;
+        NumExpediente = numExpediente;
+        AnexoDetenciones = anexoDetenciones;
+        NumAnexoDetenciones = numAnexoDetenciones;
+        AnexoUsoFuerza = anexoUsoFuerza;
+        NumAnexoUsoFuerza = numAnexoUsoFuerza;
+        AnexoVehiculos = anexoVehiculos;
+        NumAnexoVehiculo = numAnexoVehiculo;
+        AnexoArmasObjetos = anexoArmasObjetos;
+        NumAnexoArmasObjetos = numAnexoArmasObjetos;
+        AnexoEntrevista = anexoEntrevista;
+        NumAnexoEntrevista = numAnexoEntrevista;
+        AnexoLugarIntervencion = anexoLugarIntervencion;
+        NumAnexoLugarIntervencion = numAnexoLugarIntervencion;
+        AnexoNoSeEntregan = anexoNoSeEntregan;
+        IdPoliciaPrimerRespondiente = idPoliciaPrimerRespondiente;
+    }
+
+}
